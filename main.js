ceee9a28b8648daef7130b635607a0a1a721d55e
Manaswishetty1011 <92509118+Manaswishetty1011@users.noreply.github.com>
2022-05-26 22:05:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Next(){
+    window.location="geography.html"
+}
+function next1(){
+    window.location="Photos.html"
+}
+function next2(){
+    window.location="Video.html"
+}
+function next3(){
+    window.location="Website.html"
+}
+function next4(){
+    window.location="index.html"
+}
